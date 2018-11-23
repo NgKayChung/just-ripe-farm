5835f5e341756fe6c3546b47306790334e91afcf
DESKTOP-HD9D89L\WoonHan <chongwoonyaw_1997@hotmail.com>
2018-11-23 00:18:40 -0800

    added login page with singleton

---
+++ JustRipeFarm/Login.Designer.cs
@@ -0,0 +1,105 @@
+﻿namespace JustRipeFarm
+{
+    partial class LoginButton
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.LoginID = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.LoginPassword = new System.Windows.Forms.TextBox();
+            this.LoginBtn = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // LoginID
+            // 
+            this.LoginID.Location = new System.Drawing.Point(235, 88);
+            this.LoginID.Name = "LoginID";
+            this.LoginID.Size = new System.Drawing.Size(100, 20);
+            this.LoginID.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(180, 91);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(18, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "ID";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(145, 141);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(53, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Password";
+            // 
+            // LoginPassword
+            // 
+            this.LoginPassword.Location = new System.Drawing.Point(235, 138);
+            this.LoginPassword.Name = "LoginPassword";
+            this.LoginPassword.Size = new System.Drawing.Size(100, 20);
+            this.LoginPassword.TabIndex = 3;
+            // 
+            // LoginBtn
+            // 
+            this.LoginBtn.Location = new System.Drawing.Point(248, 203);
+            this.LoginBtn.Name = "LoginBtn";
+            this.LoginBtn.Size = new System.Drawing.Size(75, 23);
+            this.LoginBtn.TabIndex = 4;
+            this.LoginBtn.Text = "Login";
+            this.LoginBtn.UseVisualStyleBackColor = true;
+            this.LoginBtn.Click += new System.EventHandler(this.LoginBtn_Click);
+            // 
+            // LoginButton
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.LoginBtn);
+            this.Controls.Add(this.LoginPassword);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.LoginID);
+            this.Name = "LoginButton";
+            this.Text = "Login";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox LoginID;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox LoginPassword;
+        private System.Windows.Forms.Button LoginBtn;
+    }
+}
---
+++ JustRipeFarm/UserClass.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JustRipeFarm
+{
+    class UserClass
+    {
+        private bool loggin = false;
+        private string username, usertype, password;
+        private static UserClass instance;
+
+        private UserClass()
+        {
+
+        }
+
+        public static UserClass Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+
+                    instance = new UserClass();
+
+                }
+
+                return instance;
+            }
+        }
+
+
+       
+
+
+        public bool Loggerin { get => loggin; set => loggin = value; }
+        public string Username { get => username; set => username = value; }
+        public string Usertype { get => usertype; set => usertype = value; }
+
+    }
+}
