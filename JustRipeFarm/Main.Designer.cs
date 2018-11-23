def314d372eff6d03853c568049569de1e69aef7
NgKayChung <33350272+NgKayChung@users.noreply.github.com>
2018-11-23 12:35:15 +0800

    Add project files.

---
+++ JustRipeFarm/Main.Designer.cs
@@ -0,0 +1,193 @@
+﻿namespace JustRipeFarm
+{
+    partial class Main
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SplitContainer_Menu_Content = new System.Windows.Forms.SplitContainer();
+            this.exit_button = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.homePanel = new System.Windows.Forms.Panel();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.SplitContainer_Menu_Content)).BeginInit();
+            this.SplitContainer_Menu_Content.Panel1.SuspendLayout();
+            this.SplitContainer_Menu_Content.Panel2.SuspendLayout();
+            this.SplitContainer_Menu_Content.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // SplitContainer_Menu_Content
+            // 
+            this.SplitContainer_Menu_Content.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.SplitContainer_Menu_Content.Location = new System.Drawing.Point(0, 0);
+            this.SplitContainer_Menu_Content.Name = "SplitContainer_Menu_Content";
+            // 
+            // SplitContainer_Menu_Content.Panel1
+            // 
+            this.SplitContainer_Menu_Content.Panel1.Controls.Add(this.button7);
+            this.SplitContainer_Menu_Content.Panel1.Controls.Add(this.button6);
+            this.SplitContainer_Menu_Content.Panel1.Controls.Add(this.exit_button);
+            this.SplitContainer_Menu_Content.Panel1.Controls.Add(this.button5);
+            this.SplitContainer_Menu_Content.Panel1.Controls.Add(this.button4);
+            this.SplitContainer_Menu_Content.Panel1.Controls.Add(this.button3);
+            this.SplitContainer_Menu_Content.Panel1.Controls.Add(this.button2);
+            this.SplitContainer_Menu_Content.Panel1.Controls.Add(this.button1);
+            // 
+            // SplitContainer_Menu_Content.Panel2
+            // 
+            this.SplitContainer_Menu_Content.Panel2.Controls.Add(this.homePanel);
+            this.SplitContainer_Menu_Content.Size = new System.Drawing.Size(579, 360);
+            this.SplitContainer_Menu_Content.SplitterDistance = 108;
+            this.SplitContainer_Menu_Content.TabIndex = 0;
+            // 
+            // exit_button
+            // 
+            this.exit_button.Dock = System.Windows.Forms.DockStyle.Top;
+            this.exit_button.Location = new System.Drawing.Point(0, 200);
+            this.exit_button.Name = "exit_button";
+            this.exit_button.Size = new System.Drawing.Size(108, 40);
+            this.exit_button.TabIndex = 5;
+            this.exit_button.Text = "Shop / Wholesale";
+            this.exit_button.UseVisualStyleBackColor = true;
+            // 
+            // button5
+            // 
+            this.button5.Dock = System.Windows.Forms.DockStyle.Top;
+            this.button5.Location = new System.Drawing.Point(0, 160);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(108, 40);
+            this.button5.TabIndex = 4;
+            this.button5.Text = "Machinery";
+            this.button5.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.Dock = System.Windows.Forms.DockStyle.Top;
+            this.button4.Location = new System.Drawing.Point(0, 120);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(108, 40);
+            this.button4.TabIndex = 3;
+            this.button4.Text = "Labour";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Dock = System.Windows.Forms.DockStyle.Top;
+            this.button3.Location = new System.Drawing.Point(0, 80);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(108, 40);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "Storage";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Dock = System.Windows.Forms.DockStyle.Top;
+            this.button2.Location = new System.Drawing.Point(0, 40);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(108, 40);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Timetable";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.button1.Location = new System.Drawing.Point(0, 0);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(108, 40);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Home";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // homePanel
+            // 
+            this.homePanel.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.homePanel.Location = new System.Drawing.Point(0, 0);
+            this.homePanel.Name = "homePanel";
+            this.homePanel.Size = new System.Drawing.Size(467, 360);
+            this.homePanel.TabIndex = 0;
+            // 
+            // button6
+            // 
+            this.button6.Dock = System.Windows.Forms.DockStyle.Top;
+            this.button6.Location = new System.Drawing.Point(0, 240);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(108, 40);
+            this.button6.TabIndex = 6;
+            this.button6.Text = "Logout";
+            this.button6.UseVisualStyleBackColor = true;
+            // 
+            // button7
+            // 
+            this.button7.Dock = System.Windows.Forms.DockStyle.Top;
+            this.button7.Location = new System.Drawing.Point(0, 280);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(108, 40);
+            this.button7.TabIndex = 7;
+            this.button7.Text = "Exit";
+            this.button7.UseVisualStyleBackColor = true;
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.ClientSize = new System.Drawing.Size(579, 360);
+            this.Controls.Add(this.SplitContainer_Menu_Content);
+            this.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Name = "Main";
+            this.Text = "JustRipe Farm";
+            this.SplitContainer_Menu_Content.Panel1.ResumeLayout(false);
+            this.SplitContainer_Menu_Content.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.SplitContainer_Menu_Content)).EndInit();
+            this.SplitContainer_Menu_Content.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.SplitContainer SplitContainer_Menu_Content;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button exit_button;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Panel homePanel;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button6;
+    }
+}
+
