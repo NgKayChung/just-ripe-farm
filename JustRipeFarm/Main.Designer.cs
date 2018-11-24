0b5f53454ef3e37f388f9d145e80f022989a7b86
NgKayChung <33350272+NgKayChung@users.noreply.github.com>
2018-11-24 09:05:45 +0800

    Change interface

--- JustRipeFarm/Main.Designer.cs
+++ JustRipeFarm/Main.Designer.cs
@@ -30,131 +30,178 @@
         {
             this.SplitContainer_Menu_Content = new System.Windows.Forms.SplitContainer();
             this.exit_button = new System.Windows.Forms.Button();
-            this.button5 = new System.Windows.Forms.Button();
-            this.button4 = new System.Windows.Forms.Button();
-            this.button3 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
-            this.button1 = new System.Windows.Forms.Button();
-            this.homePanel = new System.Windows.Forms.Panel();
-            this.button6 = new System.Windows.Forms.Button();
-            this.button7 = new System.Windows.Forms.Button();
+            this.logout_button = new System.Windows.Forms.Button();
+            this.shop_button = new System.Windows.Forms.Button();
+            this.machine_button = new System.Windows.Forms.Button();
+            this.labour_button = new System.Windows.Forms.Button();
+            this.storage_button = new System.Windows.Forms.Button();
+            this.timetable_btn = new System.Windows.Forms.Button();
+            this.home_btn = new System.Windows.Forms.Button();
+            this.home_panel = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.SplitContainer_Menu_Content)).BeginInit();
             this.SplitContainer_Menu_Content.Panel1.SuspendLayout();
             this.SplitContainer_Menu_Content.Panel2.SuspendLayout();
             this.SplitContainer_Menu_Content.SuspendLayout();
+            this.home_panel.SuspendLayout();
             this.SuspendLayout();
             // 
             // SplitContainer_Menu_Content
             // 
+            this.SplitContainer_Menu_Content.BackColor = System.Drawing.SystemColors.Control;
             this.SplitContainer_Menu_Content.Dock = System.Windows.Forms.DockStyle.Fill;
             this.SplitContainer_Menu_Content.Location = new System.Drawing.Point(0, 0);
             this.SplitContainer_Menu_Content.Name = "SplitContainer_Menu_Content";
             // 
             // SplitContainer_Menu_Content.Panel1
             // 
-            this.SplitContainer_Menu_Content.Panel1.Controls.Add(this.button7);
-            this.SplitContainer_Menu_Content.Panel1.Controls.Add(this.button6);
             this.SplitContainer_Menu_Content.Panel1.Controls.Add(this.exit_button);
-            this.SplitContainer_Menu_Content.Panel1.Controls.Add(this.button5);
-            this.SplitContainer_Menu_Content.Panel1.Controls.Add(this.button4);
-            this.SplitContainer_Menu_Content.Panel1.Controls.Add(this.button3);
-            this.SplitContainer_Menu_Content.Panel1.Controls.Add(this.button2);
-            this.SplitContainer_Menu_Content.Panel1.Controls.Add(this.button1);
+            this.SplitContainer_Menu_Content.Panel1.Controls.Add(this.logout_button);
+            this.SplitContainer_Menu_Content.Panel1.Controls.Add(this.shop_button);
+            this.SplitContainer_Menu_Content.Panel1.Controls.Add(this.machine_button);
+            this.SplitContainer_Menu_Content.Panel1.Controls.Add(this.labour_button);
+            this.SplitContainer_Menu_Content.Panel1.Controls.Add(this.storage_button);
+            this.SplitContainer_Menu_Content.Panel1.Controls.Add(this.timetable_btn);
+            this.SplitContainer_Menu_Content.Panel1.Controls.Add(this.home_btn);
             // 
             // SplitContainer_Menu_Content.Panel2
             // 
-            this.SplitContainer_Menu_Content.Panel2.Controls.Add(this.homePanel);
+            this.SplitContainer_Menu_Content.Panel2.Controls.Add(this.home_panel);
             this.SplitContainer_Menu_Content.Size = new System.Drawing.Size(579, 360);
-            this.SplitContainer_Menu_Content.SplitterDistance = 108;
+            this.SplitContainer_Menu_Content.SplitterDistance = 120;
             this.SplitContainer_Menu_Content.TabIndex = 0;
             // 
             // exit_button
             // 
+            this.exit_button.BackColor = System.Drawing.Color.Green;
             this.exit_button.Dock = System.Windows.Forms.DockStyle.Top;
-            this.exit_button.Location = new System.Drawing.Point(0, 200);
+            this.exit_button.FlatAppearance.BorderColor = System.Drawing.SystemColors.Control;
+            this.exit_button.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.exit_button.ForeColor = System.Drawing.Color.White;
+            this.exit_button.Location = new System.Drawing.Point(0, 280);
             this.exit_button.Name = "exit_button";
-            this.exit_button.Size = new System.Drawing.Size(108, 40);
-            this.exit_button.TabIndex = 5;
-            this.exit_button.Text = "Shop / Wholesale";
-            this.exit_button.UseVisualStyleBackColor = true;
+            this.exit_button.Size = new System.Drawing.Size(120, 40);
+            this.exit_button.TabIndex = 7;
+            this.exit_button.Text = "Exit";
+            this.exit_button.UseVisualStyleBackColor = false;
             // 
-            // button5
+            // logout_button
             // 
-            this.button5.Dock = System.Windows.Forms.DockStyle.Top;
-            this.button5.Location = new System.Drawing.Point(0, 160);
-            this.button5.Name = "button5";
-            this.button5.Size = new System.Drawing.Size(108, 40);
-            this.button5.TabIndex = 4;
-            this.button5.Text = "Machinery";
-            this.button5.UseVisualStyleBackColor = true;
+            this.logout_button.BackColor = System.Drawing.Color.Green;
+            this.logout_button.Dock = System.Windows.Forms.DockStyle.Top;
+            this.logout_button.FlatAppearance.BorderColor = System.Drawing.SystemColors.Control;
+            this.logout_button.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.logout_button.ForeColor = System.Drawing.Color.White;
+            this.logout_button.Location = new System.Drawing.Point(0, 240);
+            this.logout_button.Name = "logout_button";
+            this.logout_button.Size = new System.Drawing.Size(120, 40);
+            this.logout_button.TabIndex = 6;
+            this.logout_button.Text = "Logout";
+            this.logout_button.UseVisualStyleBackColor = false;
             // 
-            // button4
+            // shop_button
             // 
-            this.button4.Dock = System.Windows.Forms.DockStyle.Top;
-            this.button4.Location = new System.Drawing.Point(0, 120);
-            this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(108, 40);
-            this.button4.TabIndex = 3;
-            this.button4.Text = "Labour";
-            this.button4.UseVisualStyleBackColor = true;
+            this.shop_button.BackColor = System.Drawing.Color.Green;
+            this.shop_button.Dock = System.Windows.Forms.DockStyle.Top;
+            this.shop_button.FlatAppearance.BorderColor = System.Drawing.SystemColors.Control;
+            this.shop_button.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.shop_button.ForeColor = System.Drawing.Color.White;
+            this.shop_button.Location = new System.Drawing.Point(0, 200);
+            this.shop_button.Name = "shop_button";
+            this.shop_button.Size = new System.Drawing.Size(120, 40);
+            this.shop_button.TabIndex = 5;
+            this.shop_button.Text = "Shop / Wholesale";
+            this.shop_button.UseVisualStyleBackColor = false;
             // 
-            // button3
+            // machine_button
             // 
-            this.button3.Dock = System.Windows.Forms.DockStyle.Top;
-            this.button3.Location = new System.Drawing.Point(0, 80);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(108, 40);
-            this.button3.TabIndex = 2;
-            this.button3.Text = "Storage";
-            this.button3.UseVisualStyleBackColor = true;
+            this.machine_button.BackColor = System.Drawing.Color.Green;
+            this.machine_button.Dock = System.Windows.Forms.DockStyle.Top;
+            this.machine_button.FlatAppearance.BorderColor = System.Drawing.SystemColors.Control;
+            this.machine_button.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.machine_button.ForeColor = System.Drawing.Color.White;
+            this.machine_button.Location = new System.Drawing.Point(0, 160);
+            this.machine_button.Name = "machine_button";
+            this.machine_button.Size = new System.Drawing.Size(120, 40);
+            this.machine_button.TabIndex = 4;
+            this.machine_button.Text = "Machinery";
+            this.machine_button.UseVisualStyleBackColor = false;
             // 
-            // button2
+            // labour_button
             // 
-            this.button2.Dock = System.Windows.Forms.DockStyle.Top;
-            this.button2.Location = new System.Drawing.Point(0, 40);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(108, 40);
-            this.button2.TabIndex = 1;
-            this.button2.Text = "Timetable";
-            this.button2.UseVisualStyleBackColor = true;
+            this.labour_button.BackColor = System.Drawing.Color.Green;
+            this.labour_button.Dock = System.Windows.Forms.DockStyle.Top;
+            this.labour_button.FlatAppearance.BorderColor = System.Drawing.SystemColors.Control;
+            this.labour_button.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.labour_button.ForeColor = System.Drawing.Color.White;
+            this.labour_button.Location = new System.Drawing.Point(0, 120);
+            this.labour_button.Name = "labour_button";
+            this.labour_button.Size = new System.Drawing.Size(120, 40);
+            this.labour_button.TabIndex = 3;
+            this.labour_button.Text = "Labour";
+            this.labour_button.UseVisualStyleBackColor = false;
             // 
-            // button1
+            // storage_button
             // 
-            this.button1.Dock = System.Windows.Forms.DockStyle.Top;
-            this.button1.Location = new System.Drawing.Point(0, 0);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(108, 40);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "Home";
-            this.button1.UseVisualStyleBackColor = true;
+            this.storage_button.BackColor = System.Drawing.Color.Green;
+            this.storage_button.Dock = System.Windows.Forms.DockStyle.Top;
+            this.storage_button.FlatAppearance.BorderColor = System.Drawing.SystemColors.Control;
+            this.storage_button.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.storage_button.ForeColor = System.Drawing.Color.White;
+            this.storage_button.Location = new System.Drawing.Point(0, 80);
+            this.storage_button.Name = "storage_button";
+            this.storage_button.Size = new System.Drawing.Size(120, 40);
+            this.storage_button.TabIndex = 2;
+            this.storage_button.Text = "Storage";
+            this.storage_button.UseVisualStyleBackColor = false;
             // 
-            // homePanel
+            // timetable_btn
             // 
-            this.homePanel.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.homePanel.Location = new System.Drawing.Point(0, 0);
-            this.homePanel.Name = "homePanel";
-            this.homePanel.Size = new System.Drawing.Size(467, 360);
-            this.homePanel.TabIndex = 0;
+            this.timetable_btn.BackColor = System.Drawing.Color.Green;
+            this.timetable_btn.Dock = System.Windows.Forms.DockStyle.Top;
+            this.timetable_btn.FlatAppearance.BorderColor = System.Drawing.SystemColors.Control;
+            this.timetable_btn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.timetable_btn.ForeColor = System.Drawing.Color.White;
+            this.timetable_btn.Location = new System.Drawing.Point(0, 40);
+            this.timetable_btn.Name = "timetable_btn";
+            this.timetable_btn.Size = new System.Drawing.Size(120, 40);
+            this.timetable_btn.TabIndex = 1;
+            this.timetable_btn.Text = "Timetable";
+            this.timetable_btn.UseVisualStyleBackColor = false;
             // 
-            // button6
+            // home_btn
             // 
-            this.button6.Dock = System.Windows.Forms.DockStyle.Top;
-            this.button6.Location = new System.Drawing.Point(0, 240);
-            this.button6.Name = "button6";
-            this.button6.Size = new System.Drawing.Size(108, 40);
-            this.button6.TabIndex = 6;
-            this.button6.Text = "Logout";
-            this.button6.UseVisualStyleBackColor = true;
+            this.home_btn.BackColor = System.Drawing.Color.Green;
+            this.home_btn.Dock = System.Windows.Forms.DockStyle.Top;
+            this.home_btn.FlatAppearance.BorderColor = System.Drawing.SystemColors.Control;
+            this.home_btn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.home_btn.ForeColor = System.Drawing.Color.White;
+            this.home_btn.Location = new System.Drawing.Point(0, 0);
+            this.home_btn.Name = "home_btn";
+            this.home_btn.Size = new System.Drawing.Size(120, 40);
+            this.home_btn.TabIndex = 0;
+            this.home_btn.Text = "Home";
+            this.home_btn.UseVisualStyleBackColor = false;
             // 
-            // button7
+            // home_panel
             // 
-            this.button7.Dock = System.Windows.Forms.DockStyle.Top;
-            this.button7.Location = new System.Drawing.Point(0, 280);
-            this.button7.Name = "button7";
-            this.button7.Size = new System.Drawing.Size(108, 40);
-            this.button7.TabIndex = 7;
-            this.button7.Text = "Exit";
-            this.button7.UseVisualStyleBackColor = true;
+            this.home_panel.BackColor = System.Drawing.SystemColors.Control;
+            this.home_panel.Controls.Add(this.label1);
+            this.home_panel.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.home_panel.Location = new System.Drawing.Point(0, 0);
+            this.home_panel.Name = "home_panel";
+            this.home_panel.Size = new System.Drawing.Size(455, 360);
+            this.home_panel.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label1.Location = new System.Drawing.Point(12, 14);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(58, 18);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "HOME";
             // 
             // Main
             // 
@@ -162,6 +209,7 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
             this.BackColor = System.Drawing.SystemColors.Control;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.ClientSize = new System.Drawing.Size(579, 360);
             this.Controls.Add(this.SplitContainer_Menu_Content);
             this.ForeColor = System.Drawing.SystemColors.ControlText;
@@ -172,6 +220,8 @@
             this.SplitContainer_Menu_Content.Panel2.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)(this.SplitContainer_Menu_Content)).EndInit();
             this.SplitContainer_Menu_Content.ResumeLayout(false);
+            this.home_panel.ResumeLayout(false);
+            this.home_panel.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -179,15 +229,16 @@
         #endregion
 
         private System.Windows.Forms.SplitContainer SplitContainer_Menu_Content;
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button home_btn;
+        private System.Windows.Forms.Button shop_button;
+        private System.Windows.Forms.Button machine_button;
+        private System.Windows.Forms.Button labour_button;
+        private System.Windows.Forms.Button storage_button;
+        private System.Windows.Forms.Button timetable_btn;
         private System.Windows.Forms.Button exit_button;
-        private System.Windows.Forms.Button button5;
-        private System.Windows.Forms.Button button4;
-        private System.Windows.Forms.Button button3;
-        private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.Panel homePanel;
-        private System.Windows.Forms.Button button7;
-        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button logout_button;
+        private System.Windows.Forms.Panel home_panel;
+        private System.Windows.Forms.Label label1;
     }
 }
 
