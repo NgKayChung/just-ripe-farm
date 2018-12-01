d0bccab1341a26a71bae91973afba95b854532e4
POPPINCURSE\Marcus <marcuskong015@gmail.com>
2018-12-01 12:13:06 +0800

    added stock and storage classes with database connection, plz check ah have a nice day!

---
+++ JustRipeFarm/classes/StockHandler.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+
+namespace JustRipeFarm
+{
+    class StockHandler
+    {
+        public List<Stock> FindStocksForStorage(string storage_id)
+        {
+            List<Stock> stocks = null;
+            string sqlString = "SELECT `stocks`.`stock_id`, `stocks`.`name`, `stocks`.`brand`, `stocks`.`capacity_use`, `stocks`.`stock_type`, `storage_stock`.`quantity` " +
+                "FROM `storages` " +
+                "INNER JOIN `storage_stock` " +
+	                "ON `storages`.`storage_id` = `storage_stock`.`storage_id` " +
+                "INNER JOIN `stocks` " +
+	                "ON `storage_stock`.`stock_id` = `stocks`.`stock_id` " +
+                "WHERE `storage_stock`.`storage_id` = '" + storage_id + "';";
+
+            MySqlCommand sqlCommand = new MySqlCommand(sqlString, DbConnector.Instance.getConn());
+            MySqlDataReader reader = sqlCommand.ExecuteReader();
+
+            if (reader.HasRows)
+            {
+                stocks = new List<Stock>();
+                while (reader.Read())
+                {
+                    string stockID = reader.GetString(0);
+                    string stockName = reader.GetString(1);
+                    string stockBrand = reader.GetString(2);
+                    int capacityUse = reader.GetInt32(3);
+                    string stockType = reader.GetString(4);
+                    int stockQuantity = reader.GetInt32(5);
+
+                    stocks.Add(new Stock(stockID, stockName, stockBrand, capacityUse, stockType, stockQuantity));
+                }
+            }
+
+            if (!reader.IsClosed) reader.Close();
+            sqlCommand.Dispose();
+
+            return stocks;
+        }
+
+        public List<Stock> FindStocksFilterType(string storage_id, string stock_type)
+        {
+            List<Stock> stocks = null;
+            string sqlString = "SELECT `stocks`.`stock_id`, `stocks`.`name`, `stocks`.`brand`, `stocks`.`capacity_use`, `stocks`.`stock_type`, `storage_stock`.`quantity` " +
+                "FROM `storages` " +
+                "INNER JOIN `storage_stock` " +
+                    "ON `storages`.`storage_id` = `storage_stock`.`storage_id` " +
+                "INNER JOIN `stocks` " +
+                    "ON `storage_stock`.`stock_id` = `stocks`.`stock_id` " +
+                "WHERE `storage_stock`.`storage_id` = '" + storage_id + "' " +
+                    "AND `stocks`.`stock_type` = '" + stock_type + "';";
+
+            MySqlCommand sqlCommand = new MySqlCommand(sqlString, DbConnector.Instance.getConn());
+            MySqlDataReader reader = sqlCommand.ExecuteReader();
+
+            if (reader.HasRows)
+            {
+                stocks = new List<Stock>();
+                while (reader.Read())
+                {
+                    string stockID = reader.GetString(0);
+                    string stockName = reader.GetString(1);
+                    string stockBrand = reader.GetString(2);
+                    int capacityUse = reader.GetInt32(3);
+                    string stockType = reader.GetString(4);
+                    int stockQuantity = reader.GetInt32(5);
+
+                    stocks.Add(new Stock(stockID, stockName, stockBrand, capacityUse, stockType, stockQuantity));
+                }
+            }
+
+            if (!reader.IsClosed) reader.Close();
+            sqlCommand.Dispose();
+
+            return stocks;
+        }
+    }
+}
---
+++ JustRipeFarm/classes/StockStorage.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JustRipeFarm
+{
+    class StockStorage
+    {
+        private string storageID;
+        private int totalCapacity;
+        private int usedCapacity;
+        private string status;
+
+        public StockStorage()
+        {
+
+        }
+
+        public StockStorage(string storage_id, int total_capacity, int used_capacity, string status)
+        {
+            storageID = storage_id;
+            totalCapacity = total_capacity;
+            usedCapacity = used_capacity;
+            this.status = status;
+        }
+
+        public string StorageID { get => storageID; set => storageID = value; }
+        public int TotalCapacity { get => totalCapacity; set => totalCapacity = value; }
+        public int UsedCapacity { get => usedCapacity; set => usedCapacity = value; }
+        public string Status { get => status; set => status = value; }
+    }
+}
