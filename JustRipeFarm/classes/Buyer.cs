e47af7da1ce8fe7485892d66988915005935c740
cher <shueeter97@hotmail.com>
2018-12-03 19:08:32 +0800

    buyer and product

---
+++ JustRipeFarm/classes/Buyer.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JustRipeFarm
+{
+    class Buyer
+    {
+        private string firstName;
+        private string lastName;
+        private string emailAddress;
+        private string phoneNumber;
+        private int visitedTimes;
+        private string avgVisitedTime;
+        private decimal totalSpent;
+        private string companyName;
+
+        public Buyer()
+        {
+
+        }
+
+        public Buyer(string first_name = "", string last_name = "", string email_address = "", string phone_number = "", int visited_times = 0, string avg_visit_time = "", decimal total_spent = 0, string company_name = "")
+        {
+            firstName = first_name;
+            lastName = last_name;
+            emailAddress = email_address;
+            phoneNumber = phone_number;
+            visitedTimes = visited_times;
+            avgVisitedTime = avg_visit_time;
+            totalSpent = total_spent;
+            companyName = company_name;
+        }
+
+        public string FirstName { get => firstName; set => firstName = value; }
+        public string LastName { get => lastName; set => lastName = value; }
+        public string EmailAddress { get => emailAddress; set => emailAddress = value; }
+        public string PhoneNumber { get => phoneNumber; set => phoneNumber = value; }
+        public int VisitedTimes { get => visitedTimes; set => visitedTimes = value; }
+        public string AvgVisitedTime { get => avgVisitedTime; set => avgVisitedTime = value; }
+        public decimal TotalSpent { get => totalSpent; set => totalSpent = value; }
+        public string CompanyName { get => companyName; set => companyName = value; }
+    }
+}
---
+++ JustRipeFarm/classes/Product.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JustRipeFarm
+{
+    class Product
+    {
+        private string productCode;
+        private string productName;
+        private int quantity;
+        private decimal price;
+        private bool isOnSale;
+
+        public Product()
+        {
+
+        }
+
+        public Product(string product_code = "", string product_name = "", int quantity = 0, decimal price = 0, bool is_on_sale = false)
+        {
+            productCode = product_code;
+            productName = product_name;
+            this.quantity = quantity;
+            this.price = price;
+            isOnSale = is_on_sale;
+        }
+
+        public string ProductCode { get => productCode; set => productCode = value; }
+        public string ProductName { get => productName; set => productName = value; }
+        public int Quantity { get => quantity; set => quantity = value; }
+        public decimal Price { get => price; set => price = value; }
+        public bool IsOnSale { get => isOnSale; set => isOnSale = value; }
+    }
+}
